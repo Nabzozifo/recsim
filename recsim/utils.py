74c353db1e8b3b4e8be75bf505fdf73fc62ac0b6
Mouhamed Naby Ndiaye <50000511+Nabzozifo@users.noreply.github.com>
2020-08-20 15:55:29 +0100

    Update utils.py

--- recsim/utils.py
+++ recsim/utils.py
@@ -48,6 +48,35 @@ def aggregate_video_cluster_metrics(responses, metrics, info=None):
     metrics['cluster_watch_count_no_click'] += 1
   return metrics
 
+def aggregate_video_cluster_metrics_multi(responses, metrics, info=None):
+  """Aggregates the video cluster metrics with one step responses.
+
+  Args:
+    responses: a dictionary of names, observed responses.
+    metrics: A dictionary mapping from metric_name to its value in float.
+    info: Additional info for computing metrics (ignored here)
+
+  Returns:
+    A dictionary storing metrics after aggregation.
+  """
+  del info  # Unused.
+  is_clicked = False
+  metrics['impression'] += 1
+
+  for resp in responses:
+    for response in resp:
+      if not response['click']:
+        continue
+      is_clicked = True
+      metrics['click'] += 1
+      metrics['quality'] += response['quality']
+      cluster_id = response['cluster_id']
+      metrics['cluster_watch_count_cluster_%d' % cluster_id] += 1
+
+  if not is_clicked:
+    metrics['cluster_watch_count_no_click'] += 1
+  return metrics
+
 
 def write_video_cluster_metrics(metrics, add_summary_fn):
   """Writes average video cluster metrics using add_summary_fn."""
